29455b2bedeba6ed8d2065dc57ab770536303876
Markova Yana <103419561+MarkovaY@users.noreply.github.com>
2022-05-25 13:34:20 +0200

    Add files via upload

---
+++ ConditionalStatementsAdvanced/NewHouse.java
@@ -0,0 +1,11 @@
+package ConditionalStatementsAdvanced;
+
+import java.util.Scanner;
+
+public class NewHouse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
